c8a23090e9f8d8d95444384e35153341034f8589
monumentalsquid <samuel@samuel-crawford.com>
2021-02-25 19:32:06 -0500

    Add contact and Text Input Group

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+import {v4 as uuid} from 'uuid';
+
+class AddContact extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        errors: {}
+    }
+
+    onSubmit = (dispatch, e) => {
+        e.preventDefault();
+        
+        const { name, email, phone } = this.state;
+
+        //Check for errors
+        if(name === '') {
+            this.setState({errors: { name: 'Name is required'}});
+            return;
+        }
+        if(email === '') {
+            this.setState({errors: { email: 'Email is required'}});
+            return;
+        }
+        if(phone === '') {
+            this.setState({errors: { phone: 'Phone is required'}});
+            return;
+        }
+
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            phone
+        }
+
+        dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+        this.setState({
+            name: '',
+            email: '',
+            phone: '',
+            errors: {}
+        });
+    }
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+
+        const { name, email, phone, errors } = this.state;
+
+        return (
+            <Consumer>
+                { value => {
+                    const { dispatch } = value;
+                    return (
+                        <div className="card mb-3">
+                            <div className="card-header">Add Card</div>
+                            <div className="card-body">
+                                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                                    <TextInputGroup label="Name" name="name" placeholder="Full Name" value={name} onChange={this.onChange} error={errors.name} />
+                                    <TextInputGroup label="Email" name="email" type="email" placeholder="Email Address" value={email} onChange={this.onChange} error={errors.email} />
+                                    <TextInputGroup label="Phone" name="phone" placeholder="Phone Number" value={phone} onChange={this.onChange} error={errors.phone} />
+                                    <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+                                </form>
+                            </div>
+                        </div>
+                    )
+
+                }}
+            </Consumer>
+        )
+        
+    }
+}
+
+export default AddContact;
