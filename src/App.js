61c68236aeced83f3e58f53f8a684d912d911eb1
monumentalsquid <samuel@samuel-crawford.com>
2021-02-27 15:50:58 -0500

    deploy ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact.js';
 import EditContact from './components/contacts/EditContact.js';
